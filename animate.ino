5e96a2aea645a27c46fc25409344282a96733f68
Faizaankhan3 <faizaan.khan3@gmail.com>
2016-07-16 18:04:35 +0530

    Different basic examples

---
+++ animate.ino
@@ -0,0 +1,52 @@
+void setup() {
+  //this function is use to set the pins for output/input purpose
+  //and this portion runs only once
+  pinMode(12,OUTPUT);//this function sets the 12 number pin as output and so on uptill 9
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+}
+int i=0;//this is a global variable use as an increment
+void loop() {
+  //this function runs always in a loop after the setup function
+  if(i==0)
+  {//it checks whether the value is 0 or not if it is 0 it sets the first led high and all the other low
+    digitalWrite(12,HIGH);
+    digitalWrite(11,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(9,LOW);
+    ++i;//increment i
+  }
+  delay(1000);//this function is used to hold the program for 1 sec as it is written in millisecond and it acts like an interrupt
+  if(i==1)
+  {//it checks whether the value is 1 or not if it is 1 it sets the second led high and all the other low
+    digitalWrite(12,LOW);
+    digitalWrite(11,HIGH);
+    digitalWrite(10,LOW);
+    digitalWrite(9,LOW);
+    ++i;//increment i
+  }
+  delay(1000);//this function is used to hold the program for 1 sec as it is written in millisecond and it acts like an interrupt
+  if(i==2)
+  {//it checks whether the value is 2 or not if it is 2 it sets the third led high and all the other low
+    digitalWrite(12,LOW);
+    digitalWrite(11,LOW);
+    digitalWrite(10,HIGH);
+    digitalWrite(9,LOW);
+    ++i;//increment i
+  }
+  delay(1000);//this function is used to hold the program for 1 sec as it is written in millisecond and it acts like an interrupt
+  if(i==3)
+  {//it checks whether the value is 3 or not if it is 3 it sets the fourth led high and all the other low
+    digitalWrite(12,LOW);
+    digitalWrite(11,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(9,HIGH);
+    ++i;//increment i
+  }
+  delay(1000);//this function is used to hold the program for 1 sec as it is written in millisecond and it acts like an interrupt
+  if(i==4)
+  {//it checks whether the value is 4 if it is 4 the value i is again intialised as 0
+    i=0;
+  }
+}
---
+++ fade.ino
@@ -0,0 +1,19 @@
+void setup() {
+  //this function is use to set the pins for output/input purpose
+  //and this portion runs only once
+  pinMode(11,OUTPUT);//this function sets the 12 number pin as output and so on uptill 11
+}
+int i;//this is a global variable used to fade the led
+void loop() {
+  //this function runs always in a loop after the setup function
+  for(i=0;i<255;i+=50)
+  {//this part of the program is use to increase the value of i by 50
+    analogWrite(11,i);
+    /*
+     * this function analogWrite is use to give the value to the pwm pins these are generally
+     * used for the pwm pins i.e pulse width modulation pins , over here we send pulses ranging from 
+     * 0 to 255 as we can see in the loop with a delay of 1 second.
+     */
+    delay(1000);
+  }
+}
